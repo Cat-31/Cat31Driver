d4fd18ff0c2bbf3df8eb2283fc892d584aa63111
Cat-31 <maoxianfeng@gmail.com>
2017-11-12 14:27:47 +0000

    update comment

--- device/SteeringEngineDriver.py
+++ device/SteeringEngineDriver.py
@@ -13,7 +13,7 @@ class SteeringEngine(object):
 
   def move_to(self, angle):
     duty_cycle = 2.5 + 10*angle/180
-	# 大角度转动 响应时间？
+    # 大角度转动 响应时间？
     for i in range(0,5,1):
         self.pwm.ChangeDutyCycle(duty_cycle) 
         time.sleep(0.02)                      
