dc8746fc423a1d0b5e45c2195e7975d586095526
Cat-31 <maoxianfeng@gmail.com>
2017-11-18 11:46:13 +0000

    add control module

--- device/HCSR04Test.py
+++ device/HCSR04Test.py
@@ -10,7 +10,7 @@ import HCSR04 as hcsr04
 atexit.register(GPIO.cleanup)
 
 GPIO.setmode(GPIO.BCM)
-config={'pins':{'T':23,'R':24},'voice_speed':340}
+config={'pins':{'T':23,'R':24}, 'voice_speed':340}
 print(config)
 
 ud  = hcsr04.HCSR04(config)
